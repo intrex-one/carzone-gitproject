17b61cc476c1e59d9e8d3e599b1cfe8eeb6e687c
Max Ilin <intrex.one@gmail.com>
2023-04-13 17:58:57 +0300

    Completed Taems moedel

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carzone_db',
+        'USER': 'postgres',
+        'PASSWORD': 'TePass7412/',
+        'HOST': 'localhost',
+
     }
 }
 
@@ -113,7 +117,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+ 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
@@ -123,3 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'carzone/static'),
 ] 
+
+
+
+# Media settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
+MEDIA_URL = '/media/'
